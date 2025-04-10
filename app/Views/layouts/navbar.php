c338a1c3c20a38e46f82da87fc0ee76cbf81dab8
mshofadev <mshofa09@gmail.com>
2025-04-10 21:46:15 +0700

    feat: feat: add sidebar tabs for user management, item management, and transactions
    Added tabs for:
    - User Management
    - Item Management
    - Create Transaction
    - My Transactions
    - All Transactions

---
+++ app/Views/layouts/navbar.php
@@ -0,0 +1,15 @@
+<nav class="app-header navbar navbar-expand bg-body">
+    <!--begin::Container-->
+    <div class="container-fluid">
+        <!--begin::Start Navbar Links-->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
+                    <i class="bi bi-list"></i>
+                </a>
+            </li>
+        </ul>
+        <!--end::Start Navbar Links-->
+    </div>
+    <!--end::Container-->
+</nav>

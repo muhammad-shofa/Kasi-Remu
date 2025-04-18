35ea943c1b7ba321b47cc33f22f5877ec3b9b702
mshofadev <mshofa09@gmail.com>
2025-04-18 18:32:18 +0700

    feat: add CategoryController and CategoryModel

---
+++ app/Controllers/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class CategoryController extends BaseController
+{
+   public function getCategories() {
+    
+   }
+}

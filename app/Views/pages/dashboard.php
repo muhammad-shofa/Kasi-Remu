1c6e6f9c391582dc0bee7b7809684655372484f8
mshofadev <mshofa09@gmail.com>
2025-04-21 09:20:01 +0700

    feat: change display data in main dashboard with dummy

--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -23,50 +23,55 @@
             <!-- Info boxes -->
             <div class="row">
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon text-bg-primary shadow-sm">
-                            <i class="bi bi-gear-fill"></i>
-                        </span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">CPU Traffic</span>
-                            <span class="info-box-number">
-                                10
-                                <small>%</small>
+                    <a href="/user-management" class="text-decoration-none">
+                        <div class="info-box">
+                            <span class="info-box-icon text-bg-primary shadow-sm">
+                                <i class="bi bi-person-fill"></i>
                             </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">User</span>
+                                <span class="info-box-number">
+                                    10 (dummy)
+                                </span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
+                        <!-- /.info-box -->
+                    </a>
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon text-bg-danger shadow-sm">
-                            <i class="bi bi-hand-thumbs-up-fill"></i>
-                        </span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Likes</span>
-                            <span class="info-box-number">41,410</span>
+                    <a href="/item-management" class="text-decoration-none">
+                        <div class="info-box">
+                            <span class="info-box-icon text-bg-danger shadow-sm">
+                                <i class="bi bi-box-seam-fill"></i>
+                            </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Item</span>
+                                <span class="info-box-number">300 (dummy)</span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
+                        <!-- /.info-box -->
+                    </a>
                 </div>
                 <!-- /.col -->
                 <!-- fix for small devices only -->
                 <!-- <div class="clearfix hidden-md-up"></div> -->
                 <div class="col-12 col-sm-6 col-md-3">
-                    <div class="info-box">
-                        <span class="info-box-icon text-bg-success shadow-sm">
-                            <i class="bi bi-cart-fill"></i>
-                        </span>
-                        <div class="info-box-content">
-                            <span class="info-box-text">Sales</span>
-                            <span class="info-box-number">760</span>
+                    <a href="#0" class="text-decoration-none">
+                        <div class="info-box">
+                            <span class="info-box-icon text-bg-success shadow-sm">
+                                <i class="bi bi-receipt-cutoff"></i>
+                            </span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">My Transactions</span>
+                                <span class="info-box-number">50 (dummy)</span>
+                            </div>
+                            <!-- /.info-box-content -->
                         </div>
-                        <!-- /.info-box-content -->
-                    </div>
-                    <!-- /.info-box -->
+                        <!-- /.info-box -->
+                    </a>
                 </div>
                 <!-- /.col -->
                 <div class="col-12 col-sm-6 col-md-3">
@@ -75,8 +80,8 @@
                             <i class="bi bi-people-fill"></i>
                         </span>
                         <div class="info-box-content">
-                            <span class="info-box-text">New Members</span>
-                            <span class="info-box-number">2,000</span>
+                            <span class="info-box-text">All Transactions</span>
+                            <span class="info-box-number">400 (dummy)</span>
                         </div>
                         <!-- /.info-box-content -->
                     </div>
